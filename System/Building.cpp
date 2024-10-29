88174e6d8afaee2d4a59f87b150aa5c2ee105fd9
u23616815 <u23616815@tuks.co.za>
2024-10-29 09:48:01 +0200

    Update Building.cpp
    check for compatibility

--- System/Building.cpp
+++ System/Building.cpp
@@ -4,9 +4,29 @@ using namespace std;
 
 #include "Building.h"
 #include "Client.h"
+void Building::setState(BuildingState* newState) {
+    state = newState;
+}
 
+BuildingState* Building::getState() {
+    return state;
+}
 
+int Building::getCostConsumption() {
+    return state->getMoneyCost(this);
+}
 
+int Building::getResourceConsumption(){
+	return state->getResourceCost(this);
+}
+
+int Building::getPowerConsumption() {
+    return state->getPowerConsumption(this);
+}
+
+int Building::getWaterConsumption() {
+    return state->getWaterConsumption(this);
+}
 
 
 
